624b2ba6a5c7776bdeefd9169b670b821d8da764
micheal ernest <chukwu17mc@gmail.com>
2023-05-02 13:06:38 -0700

    This is an advice generator app, html,css,js

---
+++ advice.js
@@ -0,0 +1,35 @@
+const advive_number = document.getElementById("up");
+const advice = document.getElementById("advice");
+const btn = document.querySelector(".btn");
+const URL = "https://api.adviceslip.com/advice";
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let advice = get_advice();
+  let advive_number = get_id();
+});
+function get_advice() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const word = data.slip.advice;
+      console.log(word);
+      document.getElementById("advice").textContent = `${word}`;
+    })
+    .catch(
+      (error) => (document.getElementById("advice").textContent = `Error`)
+    );
+}
+function get_id() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const id = data.slip.id;
+      console.log(id);
+      document.getElementById("up").textContent = `Advice #${id}`;
+    })
+    .catch(
+      (error) => (document.getElementById("up").textContent = `Advice #Error`)
+    );
+}
